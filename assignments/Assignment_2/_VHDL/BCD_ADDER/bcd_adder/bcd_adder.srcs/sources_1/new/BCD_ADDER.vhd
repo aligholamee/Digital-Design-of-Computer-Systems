fb9cc2dfd39bf6169c70f057caac67ce120fc46d
Ali Gholami <aligholami7596@gmail.com>
2017-10-13 11:46:38 +0330

    Now using STD_LOGIC instead of bit

--- assignments/Assignment_2/_VHDL/BCD_ADDER/bcd_adder/bcd_adder.srcs/sources_1/new/BCD_ADDER.vhd
+++ assignments/Assignment_2/_VHDL/BCD_ADDER/bcd_adder/bcd_adder.srcs/sources_1/new/BCD_ADDER.vhd
@@ -22,6 +22,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.common.all;
+use work.FULL_ADDER.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
