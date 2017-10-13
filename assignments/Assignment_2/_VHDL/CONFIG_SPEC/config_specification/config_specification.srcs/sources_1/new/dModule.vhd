a564c7ba588d4709fed8dab67002d5914ce7f9d7
Ali Gholami <aligholami7596@gmail.com>
2017-10-13 19:02:30 +0330

    use the dModuleConfig file inside our modules

--- assignments/Assignment_2/_VHDL/CONFIG_SPEC/config_specification/config_specification.srcs/sources_1/new/dModule.vhd
+++ assignments/Assignment_2/_VHDL/CONFIG_SPEC/config_specification/config_specification.srcs/sources_1/new/dModule.vhd
@@ -1,26 +1,27 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company: Amirkabir University of Technology
+-- Engineer: Ali Gholami (aligholamee)
+--
 -- Create Date: 10/13/2017 06:46:27 PM
--- Design Name: 
+-- Design Name:
 -- Module Name: dModule - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.dModuleConfig.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,12 +33,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity dModule is
---  Port ( );
+  -- Something :)
 end dModule;
 
 architecture Behavioral of dModule is
 
 begin
+  MODULE1: OTHER_MODULE
+  port map();
 
+  MODULE2: OTHER_MODULE
+  port map();
+
+  MODULE3: OTHER_MODULE
+  port map();
 
 end Behavioral;
