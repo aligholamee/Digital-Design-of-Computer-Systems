f0976506ec59318817766c316e98953795c8b37b
Ali Gholami <aligholami7596@gmail.com>
2017-10-13 12:32:15 +0330

    Port mapping was completed

--- assignments/Assignment_2/_VHDL/BCD_ADDER/bcd_adder/bcd_adder.srcs/sources_1/new/BCD_ADDER.vhd
+++ assignments/Assignment_2/_VHDL/BCD_ADDER/bcd_adder/bcd_adder.srcs/sources_1/new/BCD_ADDER.vhd
@@ -37,14 +37,28 @@ entity BCD_ADDER is
     port(
         input_1: in bcd_number;
         input_2: in bcd_number;
-        output: out bcd_number;
+        bcd_output: out bcd_number;
         carry_out: out bcd_number;
         carry_in: in bcd_number);
 end entity BCD_ADDER;
 
 architecture Structural of BCD_ADDER is
+  component FULL_ADDER
+    port(
+      A, B, CARRY_IN: in std_logic;
+      SUM, CARRY_OUT: out std_logic
+    );
+  end component;
 
+  signal carry_1, carry_2, carry_3: std_logic;
 begin
+  MODULE1: FULL_ADDER
+  port map(input_1[0], input_2[0], 0, bcd_output[0], carry_1);
 
+  MODULE2: FULL_ADDER
+    port map(input_1[1], input_2[1], carry_1, bcd_output[1], carry_2);
+
+  MODULE3: FULL_ADDER
+    port map(input_1[2], input_2[2], carry_2, bcd_output[2], carry_3);
 
 end Structural;
