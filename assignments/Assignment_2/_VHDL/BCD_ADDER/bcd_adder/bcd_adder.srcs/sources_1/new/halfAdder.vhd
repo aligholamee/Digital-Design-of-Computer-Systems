cbba84c8e9ac229739044e7197d67d666c92c5ca
Ali Gholami <aligholami7596@gmail.com>
2017-10-16 12:12:44 +0330

    Half adder entity/arc was added

---
+++ assignments/Assignment_2/_VHDL/BCD_ADDER/bcd_adder/bcd_adder.srcs/sources_1/new/halfAdder.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/16/2017 12:10:19 PM
+-- Design Name: 
+-- Module Name: halfAdder - RTL
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity HALF_ADDER is
+  port(
+    A, B: in bit;
+    SUM, CARRY: out bit);
+end entity HALF_ADDER;
+
+architecture RTL of HALF_ADDER is
+begin
+  SUM <= A xor B;
+  CARRY <= A and B;
+end architecture RTL;
+
