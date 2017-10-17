382cc05f2f0d0a51bb30c09ef5bcf68bfb962f04
Ali Gholami <aligholami7596@gmail.com>
2017-10-17 18:35:48 +0330

    Algo was found!

---
+++ assignments/Assignment_2/_VHDL/odd_even_sortnet/odd_even_sortnet.srcs/sources_1/new/_SORTER.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/17/2017 06:16:41 PM
+-- Design Name: 
+-- Module Name: sorter - Structural
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sorter is
+    generic(N: integer := 8);
+    -- algo found!
+end sorter;
+
+architecture Structural of sorter is
+
+begin
+
+
+end Structural;
