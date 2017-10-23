e1df665466e59639be5450ebefd75fea5736c2ca
Ali Gholami <aligholami7596@gmail.com>
2017-10-23 21:07:10 +0330

    Behavioral architecture of the GradeClassifier was added

--- assignments/Assignment_3/_VHDL/grade_classifier/grade_classifier.srcs/sources_1/new/grade_classifier.vhd
+++ assignments/Assignment_3/_VHDL/grade_classifier/grade_classifier.srcs/sources_1/new/grade_classifier.vhd
@@ -23,17 +23,19 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.Common.all;
+use work.common.all;
 
 entity GradeClassifier is
     port(
-        gradeInput: 
+        gradeInput: gradeType;
+        a, b, c, d: bit;
     )
 end GradeClassifier;
 
 architecture Behavioral of GradeClassifier is
-
 begin
-
-
+    a <= '1' when (gradeInput >= 51 and gradeInput < 60) else '0';
+    b <= '1' when (gradeInput >= 61 and gradeInput < 70) else '0';
+    c <= '1' when (gradeInput >= 71 and gradeInput < 85) else '0';
+    d <= '1' when (gradeInput >= 86) else '0';
 end Behavioral;
