59e72f9c072d20def56054d7fe2162738a8c0a5a
Ali Gholami <aligholami7596@gmail.com>
2017-10-25 13:41:49 +0330

    ModCounter test bench was added

---
+++ assignments/Assignment_3/_VHDL/generic_mod_counter/generic_mod_counter.srcs/sim_1/new/GMCTestBench.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: Amirkabir University of Technology
+-- Engineer: Ali Gholami(aligholamee)
+--
+-- Create Date: 10/25/2017 01:35:34 PM
+-- Design Name:
+-- Module Name: GMCTestBench - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity GMCTestBench is
+
+end GMCTestBench;
+
+architecture Behavioral of GMCTestBench is
+  component GenericModCounter is
+    port (
+        modInput: modType:= N;
+        counterOutput: integer;
+        asyncReset: std_logic;
+        clk: std_logic
+    );
+  end component;
+
+  signal ModInput: integer;
+  signal CounterOutput: integer;
+  signal CLK: std_logic;
+  signal RST: std_logic;
+begin
+  MC1: GenericModCounter port map(
+    modInput => ModInput;
+    counterOutput => CounterOutput;
+    asyncReset => RST;
+    clk => CLK
+  );
+  process
+    begin
+      ModInput <= 5;
+      wait for 500 ns;
+      ModInput <= 6;
+      wait for 500 ns;
+      ModInput <= 7;
+      wait for 500 ns; 
+end Behavioral;
