f2509d6d238a4d3ee2f1a0df333691125c947b59
Ali Gholami <aligholami7596@gmail.com>
2017-10-25 13:10:09 +0330

    Generic modCounter was added

---
+++ assignments/Assignment_3/_VHDL/generic_mod_counter/generic_mod_counter.srcs/sources_1/new/generic_mod_counter.vhd
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: Amirkabir University of Technology
+-- Engineer: Ali Gholami(aligholamee)
+-- 
+-- Create Date: 10/25/2017 12:50:27 PM
+-- Design Name: 
+-- Module Name: GenericModCounter - RTL
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.common.all;
+
+entity GenericModCounter is
+    generic N(integer := 4);
+    port (
+        modInput: integer:= N;
+        counterOutput: integer;
+        asyncReset: std_logic;
+        clk: std_logic
+    );
+end GenericModCounter;
+
+architecture RTL of GenericModCounter is
+
+begin
+    process(clk, asyncReset)
+    begin
+        if(asyncReset = 0) then
+            modInput := N;
+            counterOutput := 0;
+            asyncReset := 1;
+
+        elsif clk'event and clk = 1 then
+            counterOutput := counterOutput + modInput;
+        else 
+            -- Do nothing;
+        end if;
+    end process;
+end RTL;
