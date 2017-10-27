0dc50ffc88de2c7ad80185b5ea451c755680d0eb
Ali Gholami <aligholami7596@gmail.com>
2017-10-27 18:43:21 +0330

    Thermostat Simulator entity was added

---
+++ assignments/Assignment_3/_VHDL/thermostat_simulator/thermostat_simulator.srcs/sources_1/new/thermostat_simulator.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/27/2017 06:41:00 PM
+-- Design Name: 
+-- Module Name: Thermostat - 
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Thermostat is
+    port(
+        input_a: in integer;
+        input_b: in integer;
+        res: out boolean
+    );
+end Thermostat;
+
+architecture of Thermostat is
+
+begin
+
+
+end ;
