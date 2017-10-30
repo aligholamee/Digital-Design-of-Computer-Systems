6adb76e606aa1f02393a22e0120bf67e4b2e5af3
Ali Gholami <aligholami7596@gmail.com>
2017-10-30 17:14:11 +0330

    Remove for loops and implement the hardware directly :D

--- assignments/Assignment_3/_VHDL/fb_adder/fb_adder.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xelab  -wto b7100200da7a41f880239045e49f9b41 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot AdderTB_behav xil_defaultlib.AdderTB -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
