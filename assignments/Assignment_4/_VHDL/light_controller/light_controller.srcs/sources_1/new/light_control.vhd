611900046668ad427d1bb91f6162ceda3f56f238
Ali Gholami <aligholami7596@gmail.com>
2017-11-08 19:41:25 +0330

    COMPLETE IMPLEMENTATION IN 1 PROCESS ONLY

--- assignments/Assignment_4/_VHDL/light_controller/light_controller.srcs/sources_1/new/light_control.vhd
+++ assignments/Assignment_4/_VHDL/light_controller/light_controller.srcs/sources_1/new/light_control.vhd
@@ -26,7 +26,9 @@ entity LightControl is
     port(
         time: in integer range 0 to 24;
         sensor_input: in integer range 0 to 150;
-        light_status: out std_logic
+        light_status: out std_logic;
+        clk: in std_logic;
+        rst: in std_logic
     );
 end LightControl;
 
@@ -40,7 +42,62 @@ architecture FSM of LightControl is
     
     -- STATE SIGNAL
     signal STATE: STATE_TYPE := LIGHTS_OFF;
+
 begin
-
-
+    -- COMPLETE IMPLEMENTATION IN 1 PROCESS ONLY
+    O_P: process(clk, sensor_input, light_status, STATE)
+         begin
+            if(rising_edge(clk)) then
+                if(rst = '1') then
+                    -- CHANGE THE STATE TO THE LIGHTS_OFF MODE
+                    STATE <= LIGHTS_OFF;
+                    light_status <= '0';
+                else 
+                    -- CONTROL THE STATE CHANGING STATUS
+                     case STATE is 
+                        when LIGHTS_OFF =>
+                            if(time <= 5 and time => 1) then 
+                                STATE <= LIGHTS_OFF;
+                                light_status <= '0';
+                            elsif(time => 5 and time <= 8) then
+                                if(sensor_input > 100) then 
+                                    STATE <= LIGHTS_OFF;
+                                    light_status <= '0';
+                                else 
+                                    STATE <= LIGHTS_ON;
+                                    light_status <= '1';
+                                end if;
+                            elsif(time => 8 and time <= 17) then
+                                STATE <= LIGHTS_OFF;
+                            else
+                                if(sensor_input > 100) then 
+                                    STATE <= LIGHTS_OFF;
+                                else
+                                    STATE <= LIGHTS_ON;
+                        
+                        -- DUPLICATE
+                        when LIGHTS_ON =>
+                            if(time <= 5 and time => 1) then 
+                                STATE <= LIGHTS_OFF;
+                                light_status <= '0';
+                            elsif(time => 5 and time <= 8) then
+                                if(sensor_input > 100) then 
+                                    STATE <= LIGHTS_OFF;
+                                    light_status <= '0';
+                                else 
+                                    STATE <= LIGHTS_ON;
+                                    light_status <= '1';
+                                end if;
+                            elsif(time => 8 and time <= 17) then
+                                STATE <= LIGHTS_OFF;
+                            else
+                                if(sensor_input > 100) then 
+                                    STATE <= LIGHTS_OFF;
+                                else
+                                    STATE <= LIGHTS_ON;
+                        -- RECOVERY STATE
+                        when RECOVERY => 
+                            STATE <= LIGHTS_OFF;
+            end if;
+         end process;
 end FSM;
