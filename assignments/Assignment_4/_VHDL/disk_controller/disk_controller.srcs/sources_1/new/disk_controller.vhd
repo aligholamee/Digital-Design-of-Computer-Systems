be955d08f8a526b952391d8b6352180cb50f7a2a
Ali Gholami <aligholami7596@gmail.com>
2017-11-08 21:10:14 +0330

    FILE IMPLEMENTATION WAS ADDED

--- assignments/Assignment_4/_VHDL/disk_controller/disk_controller.srcs/sources_1/new/disk_controller.vhd
+++ assignments/Assignment_4/_VHDL/disk_controller/disk_controller.srcs/sources_1/new/disk_controller.vhd
@@ -25,9 +25,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity DiskController is
     port(
         service_port_1: in integer range 0 to 128;
+        service_request_1: in std_logic;
         service_port_2: in integer range 0 to 128;
+        service_request_2: in std_logic;
         service_port_3: in integer range 0 to 128;
+        service_request_3: in std_logic;
         service_port_4: in integer range 0 to 128;
+        service_request_4: in std_logic;
         clk: in std_logic;
         rst: in std_logic;
         file_output: out std_logic_vector(31 downto 0)
@@ -47,7 +51,26 @@ architecture FSM of DiskController is
 
     -- IMPLEMENTS THE DIRECTION OF SERVICE(0: PORT1 TO PORT4)(1: PORT4 DOWNTO PORT0)
     signal SERVICE_DIRECTION: std_logic := '0';
-begin
 
+    -- IMPLEMENTATION OF THE FILES
+    type FILE_CONTENT is array(31 downto 0) of bit;
+    type FILE is array(127 downto 0) of FILE_CONTENT;
+
+    signal FILESERVER: FILE;
+begin
+    O_P: process(clk)
+         begin 
+            if(rising_edge(clk)) then
+                if(rst = '1') then
+                    STATE <= START;
+                    file_output <= std_logic_vector(to_unsigned(0, 32));
+                else 
+                    case STATE is 
+                        when START => 
+                            if(SERVICE_DIRECTION = '0') then 
+                                if(service_request_1 = '1') then
+                                    STATE <= P1_SERVICE;
+                                    file
+         end process;
 
 end FSM;
