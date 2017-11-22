f310a969cacebcee882b9dbe6b0d371c3e35349d
Ali Gholami <aligholami7596@gmail.com>
2017-11-22 19:35:30 +0330

    File type was upgraded

--- assignments/Assignment_4/_VHDL/disk_controller/disk_controller.srcs/sources_1/new/disk_controller.vhd
+++ assignments/Assignment_4/_VHDL/disk_controller/disk_controller.srcs/sources_1/new/disk_controller.vhd
@@ -21,17 +21,18 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_numeric.all;
 
 entity DiskController is
     port(
         service_port_1: in integer range 0 to 128;
-        service_request_1: in std_logic;
+        service_request_1: inout std_logic;
         service_port_2: in integer range 0 to 128;
-        service_request_2: in std_logic;
+        service_request_2: inout std_logic;
         service_port_3: in integer range 0 to 128;
-        service_request_3: in std_logic;
+        service_request_3: inout std_logic;
         service_port_4: in integer range 0 to 128;
-        service_request_4: in std_logic;
+        service_request_4: inout std_logic;
         clk: in std_logic;
         rst: in std_logic;
         file_output: out std_logic_vector(31 downto 0)
@@ -53,16 +54,15 @@ architecture FSM of DiskController is
     signal SERVICE_DIRECTION: std_logic := '0';
 
     -- IMPLEMENTATION OF THE FILES
-    type FILE_CONTENT is array(31 downto 0) of bit;
-    type FILE is array(127 downto 0) of FILE_CONTENT;
+    type FILETYPE is array(127 downto 0) of std_logic_vector(31 downto 0);
 
-    signal FILESERVER: FILE;
+    signal FILESERVER: FILETYPE;
 begin
     O_P: process(clk)
          begin 
             if(rising_edge(clk)) then
                 if(rst = '1') then
-                    STATE <= START;
+                    STATE <= NO_SERVICE;
                     file_output <= std_logic_vector(to_unsigned(0, 32));
                 else 
                     case STATE is 
@@ -92,7 +92,7 @@ begin
 
                             else
                                 STATE <= NO_SERVICE;
-                                file_output <= '-';
+                                file_output <= "-";
                                 
                             end if;
 
@@ -132,6 +132,7 @@ begin
                                     STATE <= S4_SERVICE;
                                 else
                                     STATE <= S2_SERVICE;
+                                end if;
                             end if;
                         
                         when S4_SERVICE =>
@@ -151,5 +152,7 @@ begin
                         when RECOVERY =>
                                 STATE <= NO_SERVICE;
                     end case;
+                end if;
+            end if;
          end process;
 end FSM;
