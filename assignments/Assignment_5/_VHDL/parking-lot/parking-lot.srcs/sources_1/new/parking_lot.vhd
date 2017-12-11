979b39359ab0082a3d4e14cec7871012b4ce15d8
Ali Gholami <aligholami7596@gmail.com>
2017-12-11 12:07:53 +0330

    Parking Lot fsm was completely described with vhdl

--- assignments/Assignment_5/_VHDL/parking-lot/parking-lot.srcs/sources_1/new/parking_lot.vhd
+++ assignments/Assignment_5/_VHDL/parking-lot/parking-lot.srcs/sources_1/new/parking_lot.vhd
@@ -27,13 +27,79 @@ entity ParkingLot is
         a_input: in std_logic;
         b_input: in std_logic;
         door_cmd: out std_logic;
-        car_counter: out integer
+        car_counter: out integer;
+        clk: in std_logic;
+        rst: in std_logic
     );
 end ParkingLot;
 
 architecture Behavioral of ParkingLot is
+    -- DEFINITION OF THE STATE_TYPE
+    type STATE_TYPE is (NO_SENSE, A_SENSE, B_SENSE, RECOVERY);
+
+    -- IMPLEMENT THE WHOLE FSM USING 1 PROCESS
+    signal STATE: STATE_TYPE := NO_SENSE;
+
+    -- IMPLEMENTS THE SAFE FSM CREDINTIALS 
+    attribute safe_recovery_state: string;
+    attribute safe_recovery_state of STATE: signal is "RECOVERY";
 
 begin
+    O_P: process(clk)
+        begin
+            if(rising_edge(clk)) then
+                if(rst = '1') then
+                    STATE <= NO_SENSE;
+                else
+                    case STATE is
+                        when NO_SENSE =>
+                            if(a_input = '1' and b_input = '0') then
+                                STATE <= A_SENSE;
+                                door_cmd <= '1';
+                                car_counter <= 0;
+                            elsif(b_input = '1' and a_input = '0') then
+                                STATE <= B_SENSE;
+                                door_cmd <= '1';
+                                car_counter <= 0;
+                            else 
+                                STATE <= NO_SENSE;
+                                door_cmd <= '0';
+                                car_counter <= 0;
+                            end if;
+                        
+                        when A_SENSE => 
+                            if(a_input = '0' and b_input = '1') then
+                                STATE <= B_SENSE;
+                                door_cmd <= '1';
+                                car_counter <= car_counter + 1;
+                            elsif(a_input = '0' and b_input = '0') then
+                                STATE <= NO_SENSE;
+                                door_cmd <= '0' after 30 seconds;
+                            else
+                                STATE <= STATE;
+                            
+                            end if;
+                        
+                        when B_SENSE => 
+                            if(a_input = '1' and b_input = '0') then 
+                                STATE <= A_SENSE;
+                                counter <= counter - 1;
+                                door_cmd <= '1';
 
-
+                            elsif(a_input = '0' and b_input = '0') then 
+                                STATE <= NO_SENSE;
+                                door_cmd <= '0';
+                            
+                            else 
+                                STATE <= STATE;
+                            
+                            end if;
+                            
+                        
+                        when RECOVERY =>
+                                STATE <= NO_SENSE;
+                    end case;
+                end if;
+            end if;
+        end process;
 end Behavioral;
